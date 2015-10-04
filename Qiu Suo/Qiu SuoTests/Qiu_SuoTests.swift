33664af640c0e117f37d8727bfcb3f7a54b984c2
Ted Wei <witwiky@me.com>
2015-10-04 10:14:02 +0800

    add cocoa pods dependencies

---
+++ Qiu Suo/Qiu SuoTests/Qiu_SuoTests.swift
@@ -0,0 +1,36 @@
+//
+//  Qiu_SuoTests.swift
+//  Qiu SuoTests
+//
+//  Created by robbie on 15/10/4.
+//  Copyright © 2015年 robbie. All rights reserved.
+//
+
+import XCTest
+@testable import Qiu_Suo
+
+class Qiu_SuoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
